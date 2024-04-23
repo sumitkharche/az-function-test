c27843b7f6bd8c7f2c75e01873d7134bcce8adfe
shweta Bonde <shwetavb.18@gmail.com>
2024-04-23 21:14:08 +0530

    function111

---
+++ func-reporting-demo/Function.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace func_reporting_demo
+{
+    public class Function
+    {
+        private readonly ILogger<Function> _logger;
+
+        public Function(ILogger<Function> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("test")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
